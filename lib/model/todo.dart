423516dbdce09d4cf39f39d20fabb83d1106e1e3
Mathieu Ford <mathieu.ford@gmail.com>
2023-02-09 13:26:24 -0500

    Added Todo model

---
+++ lib/model/todo.dart
@@ -0,0 +1,15 @@
+class Todo {
+  int id;
+  String uid;
+  String action;
+  DateTime dueDate;
+  bool isWork;
+  
+  Todo(
+    this.id,
+    this.uid,
+    this.action,
+    this.dueDate,
+    this.isWork,
+  );
+}
