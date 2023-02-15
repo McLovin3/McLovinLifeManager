a742dad9a0b22b64068622ebf6e4ca8bb27ca4c5
Mathieu Ford <mathieu.ford@gmail.com>
2023-02-15 17:43:12 -0500

    Passed birthdays are now sorted at the end

--- lib/model/birthday.dart
+++ lib/model/birthday.dart
@@ -14,13 +14,29 @@ class Birthday {
   );
 
   static void sortBirthdays(List<Birthday> birthdays) {
+    DateTime currentDate = DateTime.now();
+    int cutOffIndex = 0;
+
     birthdays.sort((a, b) {
       if (a.date.month == b.date.month) {
         return a.date.day.compareTo(b.date.day);
-      } else {
-        return a.date.month.compareTo(b.date.month);
       }
+
+      return a.date.month.compareTo(b.date.month);
     });
+
+    for (int i = 0; i < birthdays.length; i++) {
+      Birthday birthday = birthdays.elementAt(i);
+
+      if (birthday.date.month <= currentDate.month &&
+          birthday.date.day < currentDate.day) {
+        cutOffIndex++;
+      }
+    }
+
+    List<Birthday> temp = birthdays.sublist(0, cutOffIndex);
+    birthdays.removeRange(0, cutOffIndex);
+    birthdays.addAll(temp);
   }
 
   static Birthday fromQueryDocumentSnapshot(
