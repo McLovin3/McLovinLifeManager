2169299064eb7b221c31bdaa570077bb59817108
Mathieu Ford <mathieu.ford@gmail.com>
2023-02-15 15:38:43 -0500

    Added function that sorts birthdays by month then day

--- lib/model/birthday.dart
+++ lib/model/birthday.dart
@@ -11,6 +11,16 @@ class Birthday {
     this.date,
   );
 
+  static void sortBirthdays(List<Birthday> birthdays) {
+    birthdays.sort((a, b) {
+      if (a.date.month == b.date.month) {
+        return a.date.day.compareTo(b.date.day);
+      } else {
+        return a.date.month.compareTo(b.date.month);
+      }
+    });
+  }
+
   static Birthday fromQueryDocumentSnapshot(
       QueryDocumentSnapshot<Map<String, dynamic>> json) {
     return Birthday(
