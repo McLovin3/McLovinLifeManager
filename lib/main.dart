5b19ee69e0450dfc25467fd4f88989acbaa8a8be
Mathieu Ford <mathieu.ford@gmail.com>
2023-02-16 16:27:00 -0500

    Refactored

--- lib/main.dart
+++ lib/main.dart
@@ -4,49 +4,28 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:mclovin_life_manager/pages/birthday_page.dart';
 import 'package:mclovin_life_manager/pages/todo_page.dart';
-import 'package:mclovin_life_manager/widgets/loading_widget.dart';
 import 'firebase_options.dart';
 
 const String email = "mathieu.ford@gmail.com";
 const String password = "password";
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'McLovin Life Manager',
-      theme: ThemeData(
-        primarySwatch: Colors.pink,
-      ),
-      home: FutureBuilder(
-          future: Firebase.initializeApp(
-            options: DefaultFirebaseOptions.currentPlatform,
-          ),
-          builder: (_, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting ||
-                snapshot.hasError) {
-              return const LoadingWidget();
-            }
-            return const MainApp();
-          }),
-    );
-  }
+  State<MyApp> createState() => _MyAppState();
 }
 
-class MainApp extends StatefulWidget {
-  const MainApp({super.key});
-
-  @override
-  State<MainApp> createState() => _MainAppState();
-}
-
-class _MainAppState extends State<MainApp> {
+class _MyAppState extends State<MyApp> {
   int _selectedPage = 0;
   static final List<Widget> _pages = <Widget>[
     TodoPage(
@@ -59,37 +38,43 @@ class _MainAppState extends State<MainApp> {
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: FirebaseAuth.instance
-          .signInWithEmailAndPassword(email: email, password: password),
-      builder: (context, snapshot) {
-        if (snapshot.hasError) {
-          return const Center(child: Text("No internet connection"));
-        }
-        return Scaffold(
-            appBar: AppBar(
-              title: const Center(child: Text("McLovin Life Manager")),
-            ),
-            body: _pages.elementAt(_selectedPage),
-            bottomNavigationBar: BottomNavigationBar(
-              items: const <BottomNavigationBarItem>[
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.list),
-                  label: "Todo",
-                ),
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.cake),
-                  label: "Birthdays",
-                ),
-              ],
-              currentIndex: _selectedPage,
-              onTap: (int index) {
-                setState(() {
-                  _selectedPage = index;
-                });
-              },
-            ));
-      },
+    return MaterialApp(
+      title: "McLovin Life Manager",
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+      ),
+      home: FutureBuilder(
+        future: FirebaseAuth.instance
+            .signInWithEmailAndPassword(email: email, password: password),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return const Center(child: Text("No internet connection"));
+          }
+          return Scaffold(
+              appBar: AppBar(
+                title: const Center(child: Text("McLovin Life Manager")),
+              ),
+              body: _pages.elementAt(_selectedPage),
+              bottomNavigationBar: BottomNavigationBar(
+                items: const <BottomNavigationBarItem>[
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.list),
+                    label: "Todo",
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.cake),
+                    label: "Birthdays",
+                  ),
+                ],
+                currentIndex: _selectedPage,
+                onTap: (int index) {
+                  setState(() {
+                    _selectedPage = index;
+                  });
+                },
+              ));
+        },
+      ),
     );
   }
 }
