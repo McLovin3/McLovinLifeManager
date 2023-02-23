f396896d0743f4ee3a649307c67620f073b6be16
mclovin <mathieu.ford@gmail.com>
2023-02-23 15:17:20 -0500

    Created HomeScaffold Component

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:light/light.dart';
 import 'package:mclovin_life_manager/pages/birthday_page.dart';
+import 'package:mclovin_life_manager/pages/home_scaffold.dart';
 import 'package:mclovin_life_manager/pages/todo_page.dart';
 import 'package:mclovin_life_manager/widgets/themes/themes.dart';
 import 'firebase_options.dart';
@@ -31,16 +32,7 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  int _selectedPage = 0;
   bool _isDarkTheme = lightLevel < lightThreshhold;
-  static final List<Widget> _pages = <Widget>[
-    TodoPage(
-        firebaseAuth: FirebaseAuth.instance,
-        firestore: FirebaseFirestore.instance),
-    BirthdayPage(
-        firebaseAuth: FirebaseAuth.instance,
-        firestore: FirebaseFirestore.instance),
-  ];
 
   @override
   Widget build(BuildContext context) {
@@ -56,42 +48,20 @@ class _MyAppState extends State<MyApp> {
           if (snapshot.hasError) {
             return const Center(child: Text("No internet connection"));
           }
-          return Scaffold(
-            appBar: AppBar(
-              title: const Center(child: Text("McLovin Life Manager")),
-              actions: [
-                IconButton(
-                  icon: Icon(_isDarkTheme ? Icons.sunny : Icons.nightlight),
-                  onPressed: () {
-                    setState(() {
-                      _isDarkTheme = !_isDarkTheme;
-                    });
-                  },
-                )
-              ],
-            ),
-            body: _pages.elementAt(_selectedPage),
-            bottomNavigationBar: BottomNavigationBar(
-              items: const <BottomNavigationBarItem>[
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.list),
-                  label: "Todo",
-                ),
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.cake),
-                  label: "Birthdays",
-                ),
-              ],
-              currentIndex: _selectedPage,
-              onTap: (int index) {
-                setState(() {
-                  _selectedPage = index;
-                });
-              },
-            ),
+          return HomeScaffold(
+            firebaseAuth: FirebaseAuth.instance,
+            firestore: FirebaseFirestore.instance,
+            changeTheme: changeTheme,
+            isDarkTheme: _isDarkTheme,
           );
         },
       ),
     );
+  }
+
+  void changeTheme() {
+    setState(() {
+      _isDarkTheme = !_isDarkTheme;
+    });
   }
 }
