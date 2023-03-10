a4db6f552e70aa08a401d5e87f5e7f0ba787e025
mclovin <mathieu.ford@gmail.com>
2023-03-09 19:52:32 -0500

    Created ItemList model

---
+++ lib/model/item_list.dart
@@ -0,0 +1,25 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class ItemList {
+  String id;
+  String ownerId;
+  String title;
+  List<String> items;
+
+  ItemList(
+    this.id,
+    this.ownerId,
+    this.title,
+    this.items,
+  );
+
+  static ItemList fromQueryDocumentSnapshot(
+      QueryDocumentSnapshot<Map<String, dynamic>> json) {
+    return ItemList(
+      json.id,
+      json.data()["ownerId"],
+      json.data()["title"],
+      json.data()["items"],
+    );
+  }
+}
