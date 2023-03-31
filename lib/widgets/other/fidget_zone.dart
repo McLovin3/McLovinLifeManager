802eb1d0642c195e5c960e786b9c9df077cdf964
mclovin <mathieu.ford@gmail.com>
2023-03-31 10:45:51 -0400

    Added a clicky button to fidget page

--- lib/widgets/other/fidget_zone.dart
+++ lib/widgets/other/fidget_zone.dart
@@ -1,3 +1,4 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 
 class FidgetZone extends StatefulWidget {
@@ -8,8 +9,47 @@ class FidgetZone extends StatefulWidget {
 }
 
 class _FidgetZoneState extends State<FidgetZone> {
+  static const clickStart = "clickStart.mp3";
+  static const clickEnd = "clickEnd.mp3";
+  final audioPlayer = AudioPlayer();
+  double circleSize = 150;
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Center(
+          child: GestureDetector(
+            onTapDown: (details) {
+              audioPlayer.play(AssetSource(clickStart));
+              setState(() {
+                circleSize = 125;
+              });
+            },
+            onTapUp: (details) {
+              audioPlayer.play(AssetSource(clickEnd));
+              setState(() {
+                circleSize = 100;
+              });
+            },
+            child: Container(
+              width: circleSize,
+              height: circleSize,
+              decoration: BoxDecoration(
+                color: Colors.blue,
+                borderRadius: BorderRadius.circular(150),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  @override
+  void dispose() {
+    audioPlayer.dispose();
+    super.dispose();
   }
 }
